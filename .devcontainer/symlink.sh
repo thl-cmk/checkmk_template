9787c562c87aa02f3152d1d71cf9bae1f507a4d3
Andreas Doehler <andreas.doehler@gmail.com>
2021-05-19 07:59:48 +0200

    removed pnp-templates from symlink and added empty folders

--- .devcontainer/symlink.sh
+++ .devcontainer/symlink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for DIR in 'agents' 'checkman' 'checks' 'doc' 'inventory' 'notifications' 'pnp-templates' 'web'; do
+for DIR in 'agents' 'checkman' 'checks' 'doc' 'inventory' 'notifications' 'web'; do
     rm -rfv $OMD_ROOT/local/share/check_mk/$DIR
     ln -sv $WORKSPACE/$DIR $OMD_ROOT/local/share/check_mk/$DIR
 done;
