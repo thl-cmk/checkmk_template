81c25097d063f896ab743d1b2d1dfdb2ca564b2c
thl-cmk <thl-cmk@outlook.com>
2023-11-25 17:50:16 +0100

    - modified to use 2.2.0-latest cloud/trial edition - modified to link directory tree instead of individual folders - added vscode extensions "ms-python.flake8", "ms-python.autopep8", "ms-python.isort", "ms-python.pylint",   "ms-python.mypy-type-checker", "ms-python.flake8", "ms-azuretools.vscode-docker", "mikoz.black-py", "eeyore.yapf",

--- .devcontainer/symlink.sh
+++ .devcontainer/symlink.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
 
-for DIR in 'agents' 'checkman' 'checks' 'doc' 'inventory' 'notifications' 'web'; do
-    rm -rfv $OMD_ROOT/local/share/check_mk/$DIR
-    ln -sv $WORKSPACE/$DIR $OMD_ROOT/local/share/check_mk/$DIR
+for DIR in 'packages' 'packages_local' 'agents' 'snmpwalks'; do
+    rm -rfv $OMD_ROOT/var/check_mk/$DIR
+    ln -sv $WORKSPACE/var/check_mk/$DIR $OMD_ROOT/var/check_mk/$DIR
 done;
 
-rm -rfv $OMD_ROOT/local/lib/check_mk/base/plugins/agent_based
-ln -sv $WORKSPACE/agent_based $OMD_ROOT/local/lib/check_mk/base/plugins/agent_based
-
-mkdir -p $OMD_ROOT/local/lib/check_mk/gui/plugins/wato/
-mkdir -p $OMD_ROOT/local/lib/check_mk/gui/plugins/wato/check_parameters
-ln -sv $WORKSPACE/agent_based_check_parameters $OMD_ROOT/local/lib/check_mk/gui/plugins/wato/check_parameters
-
-rm -rfv $OMD_ROOT/local/lib/check_mk/special_agents
-ln -sv $WORKSPACE/agent_based_special $OMD_ROOT/local/lib/check_mk/special_agents
-
-rm -rfv $OMD_ROOT/local/lib/nagios/plugins
-ln -sv $WORKSPACE/nagios_plugins $OMD_ROOT/local/lib/nagios/plugins
-
-mkdir -p $OMD_ROOT/local/lib/check_mk/base/cee/plugins
-rm -rfv $OMD_ROOT/local/lib/check_mk/base/cee/plugins/bakery
-ln -sv $WORKSPACE/bakery $OMD_ROOT/local/lib/check_mk/base/cee/plugins/bakery
-
-rm -rfv $OMD_ROOT/local/tmp
-ln -sv $WORKSPACE/temp $OMD_ROOT/local/tmp
-
+rm -rfv $OMD_ROOT/local
+ln -sv $WORKSPACE/local $OMD_ROOT/local
 source /omd/sites/cmk/.profile && echo 'cmkadmin' | /omd/sites/cmk/bin/cmk-passwd -i cmkadmin
